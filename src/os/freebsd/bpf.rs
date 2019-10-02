9c4f37312550eb663c57cedd3d501d917ff25603
Nicolas Chabbey <eprom@toor.si>
2019-10-02 14:25:16 +0200

    Added experimental BPF functions

--- src/os/freebsd/bpf.rs
+++ src/os/freebsd/bpf.rs
@@ -7,7 +7,10 @@ use std::convert::TryInto;
 // ffi
 use std::ffi::{CString};
 
-pub fn open_bpf_device() -> io::Result<(i32)> {
+// bpf_open_device() function
+//
+/// Open BPF device and return file descriptor if successful
+pub fn bpf_open_device() -> io::Result<(i32)> {
     // find an available BPF device
     for i in 0..99 {
         // print information
@@ -39,4 +42,47 @@ pub fn open_bpf_device() -> io::Result<(i32)> {
     // if all BPF devices are exhausted
     println!("Error, cannot find an available BPF device");
     return Err(io::Error::last_os_error());
+}
+
+// bpf_bind_device() function
+//
+/// Bind BPF device to a physical interface 
+pub fn bpf_bind_device(bpf_fd: i32, interface: &String) -> io::Result<()> {
+    // create physical interface cstring
+    let mut ifname = interface;
+    let ifname = interface.push_str("\0");
+
+    // create ifreq structure
+    let ifbound = ifreq {
+        ifr_name = ifname.as_bytes(),
+    };
+
+    // ioctl
+    match unsafe { ioctl(bpf_fd, BIOCSETIF, &ifbound as *const c_char) } {
+        > 0 => Ok(()),
+        _ => Err(io::Error::last_os_error()),
+    }
+}
+
+// bpf_setup_buf() function
+//
+/// Setup BPF device buffer and features
+pub fn bpf_setup_buf(bpf_fd: i32) -> io::Result<()> {
+    // create and initialize buffer_length
+    let buf_len = 1;
+
+    // activa1e immediate mode (ioctl)
+    match unsafe { ioctl(bpf_fd, BIOCIMMEDIATE, &buf_len) } {
+        < 0 => Err(io::Error::last_os_error()),
+        _ => {},
+    }
+
+    // set buffer length (ioctl)
+    match unsafe { ioctl(bpf_fd, BIOCGBLEN, &buf_len) } {
+        < 0 => Err(io::Error::last_os_error()),
+        _ => {},
+    }
+
+    // return Ok(()) if everything went successful
+    Ok(())
 }
