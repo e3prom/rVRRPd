da8042592fc53add373745c5bfa48869081c96c8
Nicolas Chabbey <eprom@toor.si>
2019-10-11 14:20:58 +0200

    Cleaned up netinet.rs code

--- src/os/freebsd/netinet.rs
+++ src/os/freebsd/netinet.rs
@@ -10,26 +10,6 @@ use std::ffi::CString;
 // FreeBSD constants
 use crate::os::freebsd::constants::*;
 
-
-// // IfReq Structure
-// // man 4 netintro
-// #[repr(C)]
-// struct IfReq {
-//     ifr_name: [u8; IF_NAMESIZE],    // interface name
-//     ifru_addr: int_sockaddr,        // address
-//     ifru_dstaddr: int_sockaddr,     // remote ptp endpoint
-//     ifru_broadaddr: int_sockaddr,   // broadcast address
-//     //ifru_buffer: ifreq_buffer,    // user supplied buffer with length (fbsd 12.0)
-//     ifru_flags: [c_short; 2],       // flags (high,low)
-//     ifru_index: c_short,            // interface index
-//     ifru_metric: c_int,             // metric
-//     ifru_mtu: c_int,                // maximum transmission unit
-//     ifru_phys: c_int,               // physical wire
-//     ifru_media: c_int,              // physical media
-//     ifru_data: c_int,               // interface data (replaced caddr_t with c_int)
-//     ifru_cap: [c_int; 2],           // capabilities (requested/current)
-// }
-
 // ifreq_buffer Structure
 #[repr(C)]
 struct ifreq_buffer {
@@ -71,62 +51,16 @@ pub fn set_ip_address(_fd: i32, ifname: &CString, ip: [u8; 4], netmask: [u8; 4])
     }
 
     // create IP address slice
-    let ip_addr_slice = &mut [0u8; 14];
+    let ip_addr_slice = &mut [0u8; 12];
     for (i, b) in ip.iter().enumerate() {
         ip_addr_slice[i] = *b;
     }
 
     // create IP netmask slice
-    let ip_netmask_slice = &mut [0u8; 14];
+    let ip_netmask_slice = &mut [0u8; 12];
     for (i, b) in netmask.iter().enumerate() {
         ip_netmask_slice[i] = *b;
     }
-
-    // // construct IfReq structure
-    // let mut ifaddr = IfReq {
-    //     ifr_name: {
-    //         let mut buf = [0u8; IF_NAMESIZE];
-    //         buf.clone_from_slice(ifname_slice);
-    //         buf
-    //     },
-    //     ifru_addr: int_sockaddr {
-    //         sa_len: 16,
-    //         sa_family: AF_INET as u8,
-    //         _pad: 0,
-    //         sa_data: {
-    //             let mut data = [0u8; 14];
-    //             data.clone_from_slice(ip_addr_slice);
-    //             data
-    //         }
-    //     },
-    //     ifru_dstaddr: int_sockaddr {
-    //         sa_len: 0,
-    //         sa_family: 0,
-    //         _pad: 0,
-    //         sa_data: [0u8; 14],
-    //     },
-    //     ifru_broadaddr: int_sockaddr {
-    //         sa_len: 0,
-    //         sa_family: 0,
-    //         _pad: 0,
-    //         sa_data: [0u8; 14],
-    //     },
-    //     // ifru_buffer: ifreq_buffer {
-    //     //     length: 0,
-    //     //     buffer: {
-    //     //         let buf = [0u8; 64];
-    //     //         &buf as *const _ as *const c_void
-    //     //     }
-    //     // },
-    //     ifru_flags: [0i16; 2],
-    //     ifru_index: 0,
-    //     ifru_metric: 0,
-    //     ifru_mtu: 1500,
-    //     ifru_phys: 0,
-    //     ifru_media: 0,
-    //     ifru_data: 0,
-    //     ifru_cap: [0i32; 2],
-    // };
 
     let mut ifaddr = IfAliasReq {
         ifr_name: {
@@ -175,12 +109,6 @@ pub fn set_ip_address(_fd: i32, ifname: &CString, ip: [u8; 4], netmask: [u8; 4])
     if res < 0 {
         return Err(io::Error::last_os_error());
     }
-
-    // // ioctl - set interface's netmask
-    // let res = unsafe { ioctl(fd, SIOCSIFNETMASK, &mut ifnetmask) };
-    // if res < 0 {
-    //     return Err(io::Error::last_os_error());
-    // }
     
     Ok(())
 } 
