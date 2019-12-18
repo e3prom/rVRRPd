cd2afa81ef05285e80d3f192c108f1465284a971
Nicolas Chabbey <eprom@toor.si>
2019-12-18 17:01:11 +0100

    Updated root index handler

--- src/api/client/router/handlers/mod.rs
+++ src/api/client/router/handlers/mod.rs
@@ -13,10 +13,27 @@ pub mod config;
 // running config scope handlers
 pub mod run;
 
-// handlers constants
-const HELLO: &str = "Hello and welcome";
-
 // index() function
-pub fn index(state: State) -> (State, &'static str) {
-    (state, HELLO)
+pub fn index(state: State) -> (State, Response<Body>) {
+    let body = r#"
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <meta charset="utf-8">
+                <title>rVRRPd Client API</title>
+            </head>
+            <body>
+                <h2>Welcome to the rVRRPd Client API HTTP Interface</h2>
+                <p>If you see this page, it means the API processes requests from you.<p>
+                <p>However, you must first <a href=/auth>authenticate</a> by sending a POST request
+                with a valid <code>user</code> and <code>password</code> key = value pair.</p>
+                <p>Once authenticated, you will receive a cookie to be used for your further
+                resources requests.</p>
+                </p>
+            </body>
+        </html>
+    "#;
+    //  HTTP body
+    let htbody = { create_response(&state, StatusCode::OK, mime::TEXT_HTML, body) };
+    return (state, htbody);
 }
--- src/api/client/router/handlers/run/protocols/mod.rs
+++ src/api/client/router/handlers/run/protocols/mod.rs
@@ -39,6 +39,16 @@ pub fn pstatic(state: State) -> (State, Response<Body>) {
     // borrow references to the Downstream API
     let down = DownstreamAPI::borrow_from(&state);
 
+    // retrieve session cookie
+    let (user, ts_since, nonce, token) = read_session_cookies(&state);
+
+    // create SessionToken
+    let mut sess = SessionToken::new();
+    sess.set_user(user);
+    sess.set_tssince(ts_since);
+    sess.set_nonce(nonce);
+    sess.set_token(token);
+
     // send a query downstream
     let sess = SessionToken::new();
     // send a query downstream
